8af391e84d44f7e58f80249a26ed54bb4dcaaf3d
NguyenHuuNgoc <cgno1210@gmail.com>
2024-01-21 17:15:56 +0700

    config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,24 @@
 require('dotenv').config(); // this is important!
 module.exports = {
-"development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-},
-"test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-},
-"production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-}
+    "development": {
+        "username": process.env.DB_USERNAME || 'u1qxeesr6dwlrgcf',
+        "password": process.env.DB_PASSWORD || 'QNBgh5ZEsybZZ5l0XoWJ',
+        "database": process.env.DB_DATABASE || 'br4zrzu3vulbsepz7tvg',
+        "host": process.env.DB_HOST || 'br4zrzu3vulbsepz7tvg-mysql.services.clever-cloud.com',
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    }
 };
