57cbd809d254ae4dd4b1ea418537e1a8cd89e262
NguyenHuuNgoc <cgno1210@gmail.com>
2024-01-21 17:06:46 +0700

    db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,8 +15,13 @@ import { Sequelize } from 'sequelize';
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const host = process.env.DB_HOST || 'br4zrzu3vulbsepz7tvg-mysql.services.clever-cloud.com'
+const database = process.env.DB_DATABASE || 'br4zrzu3vulbsepz7tvg'
+const password = process.env.DB_PASSWORD || 'QNBgh5ZEsybZZ5l0XoWJ'
+const username = process.env.DB_USERNAME || 'u1qxeesr6dwlrgcf'
+
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
   dialect: 'mysql',
   logging: false
 });
@@ -26,10 +31,10 @@ let connectDB = async () => {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
-    console.log(process.env.DB_DATABASE)
-    console.log(process.env.DB_USERNAME)
-    console.log(process.env.DB_PASSWORD)
-    console.log(process.env.DB_HOST)
+    console.log(database)
+    console.log(username)
+    console.log(password)
+    console.log(host)
     console.error('Unable to connect to the database:', error);
   }
 }
